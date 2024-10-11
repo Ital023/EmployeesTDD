f942e8d58042ae7f8a7f3089338808f3efeb72ac
Ital023 <contadoitalo911@gmail.com>
2024-10-11 18:05:43 -0300

    feat: Department Controller find all

---
+++ src/main/java/com/devsuperior/demo/controllers/DepartmentController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.DepartmentDTO;
+import com.devsuperior.demo.services.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/departments")
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService service;
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentDTO>> findAll() {
+        List<DepartmentDTO> result = service.findAll();
+        return ResponseEntity.ok(result);
+    }
+
+}
